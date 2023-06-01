bf38ec0216c23b779f547ab464dc3f9102de8f14
Liyben <soporte@liyben.com>
2023-06-01 13:35:56 +0200

    Update res_company.py

--- server_info/models/res_company.py
+++ server_info/models/res_company.py
@@ -42,87 +42,52 @@ class ResCompany(models.Model):
     )
     cpu_usage = fields.Char(
         string="Uso",
-        default=_compute_server_info,
+        compute=_compute_server_info,
         readonly=True
     )
     mem_total = fields.Char(
         string="Total",
-        default=_compute_server_info,
+        compute=_compute_server_info,
         readonly=True
     )
     mem_used = fields.Char(
         string="Uso",
-        default=_compute_server_info,
+        compute=_compute_server_info,
         readonly=True
     )
     mem_used_percent = fields.Char(
         string="Uso(%)",
-        default=_compute_server_info,
+        compute=_compute_server_info,
         readonly=True
     )
     disk_mem_total = fields.Char(
         string="Total",
-        default=_compute_server_info,
+        compute=_compute_server_info,
         readonly=True
     )
     disk_mem_used = fields.Char(
         string="Usado",
-        default=_compute_server_info,
+        compute=_compute_server_info,
         readonly=True
     )
     disk_mem_free = fields.Char(
         string="Libre",
-        default=_compute_server_info,
+        compute=_compute_server_info,
         readonly=True
     )
     disk_mem_used_percent = fields.Char(
         string="Usado(%)",
-        default=_compute_server_info,
+        compute=_compute_server_info,
         readonly=True
     )
     ip4_info = fields.Char(
         string="IPv4",
-        default=_compute_server_info,
+        compute=_compute_server_info,
         readonly=True
     )
     ip6_info = fields.Char(
         string="IPv6",
-        default=_compute_server_info,
+        compute=_compute_server_info,
         readonly=True
     )
 
-class ServerInfoSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
-
-    @api.multi
-    def _get_current_frequency(self):
-        try:
-            return self.env['res.config.settings'].search_read([], ['update_frequency'])[-1][
-                'update_frequency']
-        except Exception as ex:
-            _logger.error(f" {ex}")
-
-        return '5000'
-    
-    
-
-    update_frequency = fields.Selection(
-        string='Time between updates',
-        help='This value sets time between Server Info updates.',
-        selection=[
-            ('1000', '1 Sec'),
-            ('2000', '2 Sec'),
-            ('5000', '5 Sec'),
-            ('10000', '10 Sec'),
-            ('30000', '30 Sec'),
-            ('60000', '1 Min'),
-            ('300000', '5 Min')
-        ],
-        default=_get_current_frequency,
-        required=True
-    )
-    host_name = fields.Char(
-        default=_get_host_name,
-        readonly=True
-    )
-    
